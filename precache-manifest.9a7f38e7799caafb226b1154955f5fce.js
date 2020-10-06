470647eeeaec68cc4cc5bd1b2eacd153399d98ae
Naveenkumar05-nk <jangidnaveen05@gmail.com>
2020-10-06 23:27:30 +0530

    Updates

---
+++ precache-manifest.9a7f38e7799caafb226b1154955f5fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e062ba9984fc01abd45b334daee3d3",
+    "url": "/News-Over-Voice/index.html"
+  },
+  {
+    "revision": "363493adb8d209ef9389",
+    "url": "/News-Over-Voice/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "fbb59bbafda5e67db872",
+    "url": "/News-Over-Voice/static/js/2.5516aa3d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/News-Over-Voice/static/js/2.5516aa3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363493adb8d209ef9389",
+    "url": "/News-Over-Voice/static/js/main.e5e00fb9.chunk.js"
+  },
+  {
+    "revision": "0247438a06d51c23299e",
+    "url": "/News-Over-Voice/static/js/runtime-main.dda11fe1.js"
+  }
+]);
